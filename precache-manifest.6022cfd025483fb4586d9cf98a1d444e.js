938c37269c4c3b43b69a4e6976f9127c9608ec19
asmin <sminmgr@gmail.com>
2020-03-11 18:53:32 +0545

    Updates

---
+++ precache-manifest.6022cfd025483fb4586d9cf98a1d444e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "450e980298abb31451c92017cac94d81",
+    "url": "/trendingmovies/index.html"
+  },
+  {
+    "revision": "3227696a647ab8eed8a0",
+    "url": "/trendingmovies/static/css/main.8073f36f.chunk.css"
+  },
+  {
+    "revision": "c7df77be81a09f988e02",
+    "url": "/trendingmovies/static/js/2.20d29666.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/trendingmovies/static/js/2.20d29666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3227696a647ab8eed8a0",
+    "url": "/trendingmovies/static/js/main.fb8f4385.chunk.js"
+  },
+  {
+    "revision": "515381f6719a36cd73bd",
+    "url": "/trendingmovies/static/js/runtime-main.d4550f83.js"
+  }
+]);
